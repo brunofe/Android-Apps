1f77c9213c3be4a25f12f95a331c24e5e25d88a9
brunofe <ferraresibruno@hotmail.com>
2019-04-16 11:22:26 -0300

    saves

--- app/src/main/java/com/studio/frasedodia/MainActivity.java
+++ app/src/main/java/com/studio/frasedodia/MainActivity.java
@@ -2,12 +2,23 @@ package com.studio.frasedodia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textoNavaFrase;
+    private Button botaoNovaFrase;
+
+    private String[] frases = {"teste","teste2"};
+    private Object textoNovaFrase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textoNovaFrase;
+
     }
 }
