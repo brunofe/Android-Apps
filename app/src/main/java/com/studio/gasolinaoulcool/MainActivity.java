eb92b3860afaf6d6ddccbe743b1412e786434036
brunofe <ferraresibruno@hotmail.com>
2019-04-23 11:07:40 -0300

    saving

--- app/src/main/java/com/studio/gasolinaoulcool/MainActivity.java
+++ app/src/main/java/com/studio/gasolinaoulcool/MainActivity.java
@@ -2,12 +2,21 @@ package com.studio.gasolinaoulcool;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText preçoAlcool;
+    private EditText preçoGasolina;
+    private Button botaoVerificar;
+    private TextView textoResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pro
     }
 }
